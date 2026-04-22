ef82e10c0d8d750441ec5dd561148934221c95ec
Emptiness31 <levinaanna60@gmail.com>
2026-04-22 22:08:26 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from app.database import Base, engine
 from app import models  # ★ Новий імпорт
-from app.routers import auth
+
+from app.auth.router import router as auth_router
+from app.routes.students import router as students_router
+from app.routes.teachers import router as teachers_router
+from app.routes.admin import router as admin_router
  
 app = FastAPI(
 	title="Електронний деканат",
@@ -11,7 +15,10 @@ app = FastAPI(
 
 
 # Підключення роутерів
-app.include_router(auth.router)
+app.include_router(auth_router)
+app.include_router(students_router)
+app.include_router(teachers_router)
+app.include_router(admin_router)
  
 @app.get("/")
 def root():
