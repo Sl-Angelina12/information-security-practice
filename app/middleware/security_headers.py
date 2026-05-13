1c2a9ac6f8c5b8fd0b43844c212e7f02e4468555
Emptiness31 <levinaanna60@gmail.com>
2026-05-13 19:54:22 +0300

    Update security_headers.py

--- app/middleware/security_headers.py
+++ app/middleware/security_headers.py
@@ -9,9 +9,9 @@ class SecurityHeadersMiddleware(BaseHTTPMiddleware):
 
         response.headers["Content-Security-Policy"] = (
             "default-src 'self'; "
-            "script-src 'self' 'unsafe-inline'; "
-            "style-src 'self' 'unsafe-inline'; "
-            "img-src 'self' data:; "
+            "script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; "
+            "style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; "
+            "img-src 'self' data:https://fastapi.tiangolo.com; "
             "frame-ancestors 'none'"
         )
         response.headers["X-Frame-Options"] = "DENY"
